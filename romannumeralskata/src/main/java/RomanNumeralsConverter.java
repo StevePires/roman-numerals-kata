e8939363a9f422f02ff3bb2a434acfbf34d2aaf6
spires <spires@wemanity.com>
2018-03-29 17:38:05 +0200

    adding converter class

---
+++ romannumeralskata/src/main/java/RomanNumeralsConverter.java
@@ -0,0 +1,17 @@
+public class RomanNumeralsConverter {
+
+    private int input;
+
+    public int getInput() {
+        return this.input;
+    }
+
+    public void setInput(int input) {
+        this.input = input;
+    }
+
+    public String getResult() {
+        return null;
+    }
+
+}
